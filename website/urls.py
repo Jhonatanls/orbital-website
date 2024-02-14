43038a91f6b1ae4a6163081014d1c3e3e62f1265
Jhonatan Latorre Sierra <61359329+Jhonatanls@users.noreply.github.com>
2024-02-14 10:28:21 -0500

    Organización de todas las vistas

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( '', views.index, name='index'),
+    path( '', views.inicio, name='inicio'),
     path( 'nosotros/', views.nosotros, name='nosotros'),
     path( 'contacto/', views.contacto, name='contacto'),
     path( 'cotizar/', views.cotizar, name='cotizar'),
